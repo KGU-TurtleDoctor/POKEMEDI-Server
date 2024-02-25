134c6d914802e91afd0db22607c8f3461269460a
programofktw <thkim000317@naver.com>
2024-02-25 14:55:02 +0900

    start spring

---
+++ src/main/java/com/turtledoctor/kgu/KguApplication.java
@@ -0,0 +1,13 @@
+package com.turtledoctor.kgu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KguApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KguApplication.class, args);
+	}
+
+}
