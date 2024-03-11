e59a5fcb297aa9012f8a32121583f8f0e7a88e6a
ReadMyMemory <lame3925@gmail.com>
2024-03-11 19:46:09 +0900

    [Feat] : OAuth2 로그인을 위한 service 추가

--- src/main/java/com/turtledoctor/kgu/auth/config/SecurityConfig.java
+++ src/main/java/com/turtledoctor/kgu/auth/config/SecurityConfig.java
@@ -1,7 +1,8 @@
-package auth.config;
+package com.turtledoctor.kgu.auth.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
---
+++ src/main/java/com/turtledoctor/kgu/auth/service/CustomOAuth2UserService.java
@@ -0,0 +1,8 @@
+package com.turtledoctor.kgu.auth.service;
+
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomOAuth2UserService extends DefaultOAuth2UserServicegit  {
+}
