d3a7c9c45e41a24663000ed047153cc491214302
Jaehyun Lee <122192096+ReadMyMemory@users.noreply.github.com>
2024-04-20 16:30:45 +0900

    [Fix] : deploy.sh 백업 설정.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,27 +2,27 @@
 BUILD_JAR=$(ls /home/ubuntu/app/kgu-0.0.1-SNAPSHOT.jar)
 JAR_NAME=$(basename $BUILD_JAR)
 
-echo "> 현재 시간: $(date)" >> /home/ubuntu/app/deploy.log
+echo "> 현재 시간: $(date)" >> /home/ubuntu/app/log/deploy.log
 
-echo "> build 파일명: $JAR_NAME" >> /home/ubuntu/app/deploy.log
+echo "> build 파일명: $JAR_NAME" >> /home/ubuntu/app/log/deploy.log
 
-echo "> build 파일 복사" >> /home/ubuntu/app/deploy.log
-DEPLOY_PATH=/home/ubuntu/app/
+echo "> build 파일 복사" >> /home/ubuntu/app/log/deploy.log
+DEPLOY_PATH=/home/ubuntu/app/deploy
 cp $BUILD_JAR $DEPLOY_PATH
 
-echo "> 현재 실행중인 애플리케이션 pid 확인" >> /home/ubuntu/app/deploy.log
+echo "> 현재 실행중인 애플리케이션 pid 확인" >> /home/ubuntu/app/log/deploy.log
 CURRENT_PID=$(pgrep -f $JAR_NAME)
 
 if [ -z $CURRENT_PID ]
 then
-  echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다." >> /home/ubuntu/app/deploy.log
+  echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다." >> /home/ubuntu/app/log/deploy.log
 else
-  echo "> kill -9 $CURRENT_PID" >> /home/ubuntu/app/deploy.log
+  echo "> kill -9 $CURRENT_PID" >> /home/ubuntu/app/log/deploy.log
   sudo kill -9 $CURRENT_PID
   sleep 5
 fi
 
 
 DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
-echo "> DEPLOY_JAR 배포"    >> /home/ubuntu/app/deploy.log
-sudo nohup java -jar $DEPLOY_JAR >> /home/ubuntu/app/deploy.log 2>/home/ubuntu/app/deploy_err.log &
+echo "> DEPLOY_JAR 배포"    >> /home/ubuntu/app/log/deploy.log
+sudo nohup java -jar $DEPLOY_JAR >> /home/ubuntu/app/log/deploy.log 2>/home/ubuntu/app/log/deploy_err.log &
