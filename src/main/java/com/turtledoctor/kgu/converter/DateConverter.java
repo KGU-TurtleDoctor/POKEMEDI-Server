5f3134c3ece797b08ed21e510b9ba6ac77a73764
programofktw <thkim000317@naver.com>
2024-05-03 14:19:42 +0900

    [Fix] DateConverter edit

--- src/main/java/com/turtledoctor/kgu/converter/DateConverter.java
+++ src/main/java/com/turtledoctor/kgu/converter/DateConverter.java
@@ -1,14 +1,19 @@
 package com.turtledoctor.kgu.converter;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 
+@Slf4j
 public class DateConverter {
     public static String ConverteDate(LocalDateTime localDateTime){
+        log.info(localDateTime.toString());
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy.MM.dd HH:mm");
         ZonedDateTime zonedDateTime= localDateTime.atZone(ZoneId.of("Asia/Seoul"));
+        log.info(zonedDateTime.toString());
         // LocalDateTime 객체를 문자열로 변환합니다.
         String formattedDateTime = zonedDateTime.format(formatter);
 
