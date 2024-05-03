7da7ca01a0baaf0dc158a49ddddbd53d5f846857
programofktw <thkim000317@naver.com>
2024-05-03 14:13:09 +0900

    [Fix] LocalDateTime을 ZoneDateTime으로 변경

--- src/main/java/com/turtledoctor/kgu/converter/DateConverter.java
+++ src/main/java/com/turtledoctor/kgu/converter/DateConverter.java
@@ -2,14 +2,15 @@ package com.turtledoctor.kgu.converter;
 
 import java.time.LocalDateTime;
 import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class DateConverter {
     public static String ConverteDate(LocalDateTime localDateTime){
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy.MM.dd HH:mm");
-        localDateTime.atZone(ZoneId.of("Asia/Seoul"));
+        ZonedDateTime zonedDateTime= localDateTime.atZone(ZoneId.of("Asia/Seoul"));
         // LocalDateTime 객체를 문자열로 변환합니다.
-        String formattedDateTime = localDateTime.format(formatter);
+        String formattedDateTime = zonedDateTime.format(formatter);
 
         return formattedDateTime;
     }
