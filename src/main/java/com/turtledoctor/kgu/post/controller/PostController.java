9a6ba1499e173b02d18cd10420e8b3f8b0914f68
ReadMyMemory <lame3925@gmail.com>
2024-05-04 00:31:29 +0900

    [Add] : 이미지 업로드를 위한 의존성 추가, api 작성 예정

--- src/main/java/com/turtledoctor/kgu/post/controller/PostController.java
+++ src/main/java/com/turtledoctor/kgu/post/controller/PostController.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
@@ -95,6 +96,12 @@ public class PostController {
                 .build();
         return ResponseEntity.ok().body(responseDTO);
     }
+
+    @PostMapping("/image/upload")
+    public ResponseEntity<ResponseDTO> postImageUpload(
+            @CookieValue(name = "Authorization") String author,
+            @RequestPart (value = "file") MultipartFile multipartFile,
+
 
 
 }
