bf9d33388bb4ddfbebd32310ab277074fcb404ae
honey720 <honey720@naver.com>
2024-05-17 11:22:52 +0900

    [Feat] : Image 엔티티 구현

---
+++ src/main/java/com/turtledoctor/kgu/entity/Image.java
@@ -0,0 +1,28 @@
+package com.turtledoctor.kgu.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Image {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String fileName;
+
+    @Column(nullable = false)
+    private String filePath;
+
+    @ManyToOne(optional = false, fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id")
+    private Post post;
+
+    //이미지가 필요한 엔티티와 ManyToOne으로 매핑시킬 것.
+}
