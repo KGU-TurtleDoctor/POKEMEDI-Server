89e51e1ae00faba609f3aec6de8126007d7e339c
ReadMyMemory <lame3925@gmail.com>
2024-05-22 16:25:29 +0900

    [Feat] : 이미지 presigned-url 업로드 api 1차 구현 완료

--- src/main/java/com/turtledoctor/kgu/upload/controller/uploadController.java
+++ src/main/java/com/turtledoctor/kgu/upload/controller/uploadController.java
@@ -1,4 +1,42 @@
 package com.turtledoctor.kgu.upload.controller;
 
-public class uploadController {
+import com.turtledoctor.kgu.response.ResponseDTO;
+import com.turtledoctor.kgu.upload.dto.ImageUploadDTO;
+import com.turtledoctor.kgu.upload.service.UploadService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/api/upload")
+@RequiredArgsConstructor
+@Slf4j
+public class UploadController {
+
+    private final UploadService uploadService;
+
+    @GetMapping ("/")
+    public ResponseEntity<ResponseDTO> getPreSignedUrlList(
+            @RequestParam("extension") String extension,
+            @RequestParam("using") String uploadFolder,
+            @RequestParam("number") int urlNumber
+    ) {
+        ImageUploadDTO imageUploadDTO = ImageUploadDTO.builder()
+                .extension(extension)
+                .uploadFolder(uploadFolder)
+                .urlNumber(urlNumber)
+                .build();
+
+        ResponseEntity<ResponseDTO> response = ResponseEntity.ok().body(ResponseDTO.builder()
+                .isSuccess(true)
+                .stateCode(200)
+                .result(uploadService.getPreSignedUrl(imageUploadDTO))
+                .build());
+
+        return response;
+    }
 }
---
+++ src/main/java/com/turtledoctor/kgu/upload/dto/ImageUploadDTO.java
@@ -0,0 +1,15 @@
+package com.turtledoctor.kgu.upload.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+public class ImageUploadDTO {
+    String extension;
+    String uploadFolder;
+    int urlNumber;
+}
